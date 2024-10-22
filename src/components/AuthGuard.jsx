9fa0598892a4f07c91a86431b66d79475bf9c9d2
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-10-22 15:42:36 +0500

    n

--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext'; // Adjust the import according to your project structure
+import { useAuth } from '../context/AuthContext'; 
 
 const AuthGuard = ({ children }) => {
-  const { user } = useAuth(); // Assuming you have a context that provides user info
+  const { user } = useAuth(); 
 
   return user ? children : <Navigate to="/signin" />;
 };
